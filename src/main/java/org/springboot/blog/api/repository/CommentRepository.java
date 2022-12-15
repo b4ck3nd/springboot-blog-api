9911187bafb09484ed36b9a16762db81de3f9842
sy5t3m <b4ck3nd@protonmail.com>
2022-12-15 21:17:35 +0300

    commentrepository

---
+++ src/main/java/org/springboot/blog/api/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package org.springboot.blog.api.repository;
+
+import org.springboot.blog.api.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+}
