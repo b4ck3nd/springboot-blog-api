b3259866b8a78347255b14a06830807a44ef956d
sy5t3m <b4ck3nd@protonmail.com>
2022-12-17 22:33:52 +0300

    postservice

---
+++ src/main/java/org/springboot/blog/api/service/post/PostService.java
@@ -0,0 +1,17 @@
+package org.springboot.blog.api.service.post;
+
+import org.springboot.blog.api.dto.post.PostCreateRequestDto;
+import org.springboot.blog.api.dto.post.PostResponseDto;
+import org.springboot.blog.api.dto.post.PostUpdateDto;
+
+import java.util.List;
+
+public interface PostService {
+    void add(PostCreateRequestDto dto);
+    List<PostResponseDto> findAll(int pageNo,String sortBy,String sortDir);
+    PostResponseDto findById(long id);
+    PostResponseDto updateById(long id, PostUpdateDto dto);
+    void deleteById(long id);
+
+
+}
---
+++ src/main/java/org/springboot/blog/api/service/post/PostServiceImpl.java
@@ -0,0 +1,42 @@
+package org.springboot.blog.api.service.post;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springboot.blog.api.dto.post.PostCreateRequestDto;
+import org.springboot.blog.api.dto.post.PostResponseDto;
+import org.springboot.blog.api.dto.post.PostUpdateDto;
+import org.springboot.blog.api.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+    private final PostRepository postRepository;
+
+    @Override
+    public void add(PostCreateRequestDto dto) {
+        
+    }
+
+    @Override
+    public List<PostResponseDto> findAll(int pageNo, String sortBy, String sortDir) {
+        return null;
+    }
+
+    @Override
+    public PostResponseDto findById(long id) {
+        return null;
+    }
+
+    @Override
+    public PostResponseDto updateById(long id, PostUpdateDto dto) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(long id) {
+
+    }
+}
