4f508fdbee7d4648e7c9d5012e688337d5367b26
sy5t3m <b4ck3nd@protonmail.com>
2023-01-02 21:18:30 +0300

    enablemethodsecurity annotations and security config

--- src/main/java/org/springboot/blog/api/config/SecurityConfig.java
+++ src/main/java/org/springboot/blog/api/config/SecurityConfig.java
@@ -1,11 +1,58 @@
 package org.springboot.blog.api.config;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springboot.blog.api.config.jwt.JwtAuthenticationEntryPoint;
+import org.springboot.blog.api.config.jwt.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests(
+                        authorize -> {
+                            authorize.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
+                                    .requestMatchers("/api/auth/**").permitAll()
+                                    .anyRequest().authenticated();
+                        }
+                )
+                .exceptionHandling(exception -> {
+                    exception.authenticationEntryPoint(jwtAuthenticationEntryPoint);
+                })
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                });
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+
+
 
 }
